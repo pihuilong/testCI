7b6c76867d11223f0a8e212e6798dd279a577cda
pihuilong <pihuilong@qq.com>
2020-03-09 13:41:24 +0800

    feat: setup ingress & metallb

--- app/src/bin/www.js
+++ app/src/bin/www.js
@@ -5,7 +5,7 @@ const port = 3000
 
 const requestHandler = (request, response) => {
   console.log(request.url)
-  response.end('Hello Node.js Server!!')
+  response.end('Hello Node.js Server!! <h1>This site belongs to Hiram Pierce, all right reserved!</h1>')
 }
 
 const server = http.createServer(requestHandler)
