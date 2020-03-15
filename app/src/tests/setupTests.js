2d93414f3c05677c5ea6313e23ceb65ced134f2f
pihuilong <pihuilong@qq.com>
2020-03-15 15:39:32 +0800

    feat: setup jest config

---
+++ app/src/tests/setupTests.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+console.log(
+  "A list of paths to modules that run some code to configure or set up the testing framework before each test file in the suite is executed. Since setupFiles executes before the test framework is installed in the environment, this script file presents you the opportunity of running some code immediately after the test framework has been installed in the environment."
+);
+console.log(
+  "Details see: https://jestjs.io/docs/en/configuration#setupfilesafterenv-array"
+);
